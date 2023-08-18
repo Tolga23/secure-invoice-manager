516887fbf147c291e2bb05a038046508b08be16e
Tolga <10477308+Tolga23@users.noreply.github.com>
2023-08-18 18:35:06 +0300

    initial commit

---
+++ src/main/java/com/thardal/secureinvoicemanager/SecureinvoicemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.thardal.secureinvoicemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureinvoicemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureinvoicemanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thardal/secureinvoicemanager/SecureinvoicemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thardal.secureinvoicemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureinvoicemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
