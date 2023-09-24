6fc9119e310fa490665f2323a52b1c27421eaf45
Tolga <10477308+Tolga23@users.noreply.github.com>
2023-09-24 20:20:42 +0300

    Improved access control in SecurityConfig
    Updated the SecurityConfig file to enhance access control for different endpoints.
    Added specific role-based permissions for DELETE operations on user and customer resources.
    Also, ensured that public URLs remain accessible to all users.

--- src/main/java/com/thardal/secureinvoicemanager/security/SecurityConfig.java
+++ src/main/java/com/thardal/secureinvoicemanager/security/SecurityConfig.java
@@ -1,6 +1,12 @@
 package com.thardal.secureinvoicemanager.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -10,8 +16,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private static final String[] PUBLIC_URLS = {};
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -30,11 +42,41 @@ public class SecurityConfig {
         return new InMemoryUserDetailsManager(user);
     }
 
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest()
+//                        .permitAll())
+//                .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable());
+//        return http.build();
+//    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest()
-                        .permitAll())
-                .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable());
+        http
+                .csrf().disable()
+                .cors().disable()
+                .sessionManagement().sessionCreationPolicy(STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(PUBLIC_URLS).permitAll()
+                .antMatchers(HttpMethod.DELETE, "/user/delete/**").hasAnyAuthority("DELETE:USER")
+                .antMatchers(HttpMethod.DELETE, "/customer/delete/**").hasAnyAuthority("DELETE:CUSTOMER")
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling()
+                .accessDeniedHandler(null)
+                .authenticationEntryPoint(null);
+
+
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(null);
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return new ProviderManager(authProvider);
     }
 }
