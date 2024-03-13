508e9345cb9a8b5e57f6612caa6c97fa78fb23fe
Tolga <10477308+Tolga23@users.noreply.github.com>
2024-03-14 01:21:21 +0300

    Add customer and invoice entities

---
+++ src/main/java/com/thardal/secureinvoicemanager/customer/entity/Customer.java
@@ -0,0 +1,32 @@
+package com.thardal.secureinvoicemanager.customer.entity;
+
+import com.thardal.secureinvoicemanager.base.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Collection;
+
+@Entity
+@Data
+public class Customer extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "customer_seq")
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+    private String type;
+    private String status;
+    private String imageUrl;
+    /**
+     * This is a One-to-Many relationship between Customer and Invoice entities.
+     * It is mapped by the 'customer' field in the Invoice entity.
+     * FetchType is LAZY which means the invoices are fetched only when they are accessed for the first time.
+     * CascadeType is ALL, so all operations (persist, merge, refresh, remove, detach) that happen on Customer will also cascade to the invoices.
+     */
+    @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Collection<Invoice> invoices;
+
+}
---
+++ src/main/java/com/thardal/secureinvoicemanager/customer/entity/Invoice.java
@@ -0,0 +1,32 @@
+package com.thardal.secureinvoicemanager.customer.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.thardal.secureinvoicemanager.base.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Invoice extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "invoice_seq")
+    private Long id;
+    private String invoiceNumber;
+    private String services;
+    private String status;
+    private Double total;
+
+    /**
+     * This is a Many-to-One relationship between Invoice and Customer entities.
+     * The 'customer' field in the Invoice entity is joined by the 'customer_id' column in the Customer entity.
+     * The 'customer_id' column cannot be null, meaning every invoice must be associated with a customer.
+     * The @JsonIgnore annotation is used to prevent circular references during serialization,
+     * which means the customer field will not be included when an Invoice entity is converted to JSON.
+     */
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    @JsonIgnore
+    private Customer customer;
+
+}
