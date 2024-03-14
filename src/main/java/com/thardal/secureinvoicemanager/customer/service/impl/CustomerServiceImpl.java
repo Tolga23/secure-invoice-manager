34578909b46c326c4c7622584e50b6c853f42e6c
Tolga <10477308+Tolga23@users.noreply.github.com>
2024-03-15 01:17:19 +0300

    Add CustoemrServiceImpl
    According to what has been done in user package before, different implementation approach is used for customer

---
+++ src/main/java/com/thardal/secureinvoicemanager/customer/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,82 @@
+package com.thardal.secureinvoicemanager.customer.service.impl;
+
+import com.thardal.secureinvoicemanager.customer.entity.Customer;
+import com.thardal.secureinvoicemanager.customer.entity.Invoice;
+import com.thardal.secureinvoicemanager.customer.repository.CustomerRepository;
+import com.thardal.secureinvoicemanager.customer.repository.InvoiceRepository;
+import com.thardal.secureinvoicemanager.customer.service.CustomerService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.data.domain.PageRequest.of;
+
+@Service
+@Transactional
+@Slf4j
+public class CustomerServiceImpl implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final InvoiceRepository invoiceRepository;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository, InvoiceRepository invoiceRepository) {
+        this.customerRepository = customerRepository;
+        this.invoiceRepository = invoiceRepository;
+    }
+
+    @Override
+    public Customer createCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    @Override
+    public Customer updateCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    @Override
+    public Page<Customer> getCustomers(int page, int size) {
+        return customerRepository.findAll(of(page,size));
+    }
+
+    @Override
+    public Iterable<Customer> getCustomers() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Customer getCustomerById(Long id) {
+        return customerRepository.findById(id).get();
+    }
+
+    @Override
+    public Page<Customer> searchCustomer(String name, int page, int size) {
+        return customerRepository.findByNameContaining(name, of(page,size));
+    }
+
+    @Override
+    public Invoice createInvoice(Invoice invoice) {
+        invoice.setInvoiceNumber(RandomStringUtils.randomAlphanumeric(8).toUpperCase());
+        return invoiceRepository.save(invoice);
+    }
+
+    @Override
+    public Page<Invoice> getInvoices(int page, int size) {
+        return invoiceRepository.findAll(of(page,size));
+    }
+
+    @Override
+    public void addInvoiceToCustomer(Long customerId, Invoice invoice) {
+        invoice.setInvoiceNumber(RandomStringUtils.randomAlphanumeric(8).toUpperCase());
+        Customer customer = customerRepository.findById(customerId).get();
+        invoice.setCustomer(customer);
+        invoiceRepository.save(invoice);
+    }
+
+    @Override
+    public Invoice getInvoiceById(Long invoiceId) {
+        return null;
+    }
+}
