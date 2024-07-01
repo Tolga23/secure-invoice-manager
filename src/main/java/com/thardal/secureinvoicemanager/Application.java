377d0e0b54735a58cfc82acd9ed1a48e7569d494
Tolga Hardal <hardaltolga@gmail.com>
2024-07-01 17:53:16 +0300

    Update cors url

--- src/main/java/com/thardal/secureinvoicemanager/Application.java
+++ src/main/java/com/thardal/secureinvoicemanager/Application.java
@@ -22,7 +22,7 @@ public class Application {
         UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:3000"));
+        corsConfiguration.setAllowedOrigins(List.of("https://secure-invoice-manager-production.up.railway.app","http://localhost"));
         //corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Jwt-Token", "Authorization", "Origin", "Accept", "X-Requested-With",
